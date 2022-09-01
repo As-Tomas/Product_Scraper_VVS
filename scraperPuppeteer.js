fe992b515976ea35fa55b3306729cd6532355173
Bance <kalianasu@gmail.com>
2022-09-01 10:12:32 +0200

    pictures cover and gallery

--- scraperPuppeteer.js
+++ scraperPuppeteer.js
@@ -7,6 +7,7 @@ async function scrapeProductPage(url) {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
+    await page.setViewport({ width: 1030, height: 768 });
 
     const allAdds = [];
 
@@ -17,13 +18,51 @@ async function scrapeProductPage(url) {
     const text = await el.getProperty('textContent');
     const productName = await text.jsonValue();
 
+    //cover picture
     const [el2] = await page.$x('//*[@id="content-container"]/main/div[2]/div[2]/div[1]/div[1]/div/div[1]/div/div/div[1]/div/img');
     const src = await el2.getProperty('src');
-    const picture1 = await src.jsonValue();
+    const coverPic = await src.jsonValue();
     
-    const images = await page.$$eval("img", (imgs) => {
-        return imgs.map((x) => x.src);
-    });
+    //returns all pictures
+    // const images = await page.$$eval("img", (imgs) => {
+    //     return imgs.map((x) => x.src);
+    // });
+
+    //--- works but returns url without apendix
+    // try {
+    //     const elPic1 = (await page.$x('/html/body/div/div/div/div[1]/main/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/button[2]'))[0];
+    //     let v = await page.$eval("img", elPic1 => elPic1.getAttribute("src"))
+    // } catch (error) {
+        
+    // }
+
+    let pic1 = '';
+    try {
+        const [elPic1] = await page.$x('//*[@id="content-container"]/main/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/button[1]/div/img');
+        const srcel1 = await elPic1.getProperty('src');
+        pic1 = await srcel1.jsonValue();
+    } catch (error) {}
+    
+    let pic2 = '';
+    try {
+        const [elPic2] = await page.$x('//*[@id="content-container"]/main/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/button[2]/div/img');
+        const srcel2 = await elPic2.getProperty('src');
+        pic2 = await srcel2.jsonValue();
+    } catch (error) {}
+
+    let pic3 = '';
+    try {
+        const [elPic3] = await page.$x('//*[@id="content-container"]/main/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/button[3]/div/img');
+        const srcel3 = await elPic3.getProperty('src');
+        pic3 = await srcel3.jsonValue();       
+    } catch (error) {}
+
+    let pic4 = '';
+    try {
+        const [elPic4] = await page.$x('//*[@id="content-container"]/main/div[2]/div[2]/div[1]/div[1]/div/div[2]/div/div/button[4]/div/img');
+        const srcel4 = await elPic4.getProperty('src');
+        pic4 = await srcel4.jsonValue();  
+    } catch (error) {}
 
     const [elbrand] = await page.$x('//*[@id="content-container"]/main/div[2]/div[2]/div[1]/div[2]/section/hgroup/div[1]/p',);
     const brandText = await elbrand.getProperty('textContent');
@@ -118,13 +157,14 @@ async function scrapeProductPage(url) {
     const href = await el4.getProperty('href');
     const documentsLink = await href.jsonValue();
 
+    
+    
 
     browser.close();
-    console.log({productName, picture1, images, brand, productNumber, ShortDescription,
-        collumnName, description, collumnName2, specifications, etim, collumnName4, documentstextContent, documentsLink
+    console.log({productName, coverPic, pic1, pic2, pic3, pic4, brand, productNumber, ShortDescription,
+        collumnName, description, collumnName2, specifications, etim, collumnName4, documentstextContent, documentsLink, 
     });
 }
 
 
 scrapeProductPage(siteLink.productLink);
-
